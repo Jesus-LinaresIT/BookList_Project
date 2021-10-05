abd7b70556e598185d6927b69fb9ed9aceb11b62
JesusLinares01 <alberto.salinas@asesoriait.com>
2021-10-05 16:44:43 -0600

    Added validation and set custom alert

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
 // UI Book List constructor
 class Book{
-   constructor(title, author, isbn){
-      this.title = title;
-      this.author = author;
-      this.isbn = isbn;
+   constructor(){
    }
 
    // UI Constructor
-   addBook(title, author, isbn){
+   addBook(book){
       const list = document.getElementById('book-list');
       // Create tr element
       const row = document.createElement('tr');
       //insert cols
       row.innerHTML = `
-         <td>${title}</td>
-         <td>${author}</td>
-         <td>${isbn}</td>
+         <td>${book.title}</td>
+         <td>${book.author}</td>
+         <td>${book.isbn}</td>
          <td><a href="#">X</a></td>
       `;
 
       list.appendChild(row);
    }
+
+   // Show Alert
+   showAlert(msg, className){
+      // Create div
+      const div = document.createElement('div');
+      div.className = className;
+      div.appendChild(document.createTextNode(msg));
+
+      // Get parent element
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#book-form');
+      // insert alert
+      container.insertBefore(div, form);
+
+      setTimeout(()=> {
+         document.querySelector('.alert').remove();
+      }, 3000)
+   }
+
 
    // Clear field
    clearFields = () => {
@@ -30,24 +46,33 @@ class Book{
    }
 }
 
+
 // Event Listener
 document.getElementById('book-form').addEventListener('submit', e =>{
    //Get form values
-   const title = document.getElementById('title').value,
-         author = document.getElementById('author').value,
-         isbn = document.getElementById('isbn').value;
+   const book = {
+      'title': document.getElementById('title').value,
+      'author': document.getElementById('author').value,
+      'isbn': document.getElementById('isbn').value
+   };
+
    // instantiate book
-   const book = new Book();
-   // Add book to list
+   const bookService = new Book();
+
    try{
-      book.addBook(title, author, isbn);
+      if(book.title !== '' || book.author !== '' || book.isbn !== ''){
+         // Add book to list
+         bookService.addBook(book);
+         bookService.showAlert('Add Book Success', 'alert alert-success')
+      }else{
+         bookService.showAlert('Please fill in all fields.', 'alert alert-danger');
+      }
    }
    catch(err){
       console.log(err);
    }
-
    // Clear field values
-   book.clearFields();
+   bookService.clearFields();
 
    e.preventDefault();
 })
